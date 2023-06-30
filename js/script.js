8ff60332a2fba250fcd53b63f5a0c99b3faa4db3
Ranjeet <ranjeet0001@gmail.com>
2023-06-30 15:50:08 +0530

    backdrop blur

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const observer = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
+    // console.log(ent);
     if (!ent.isIntersecting) document.body.classList.add("sticky");
     if (ent.isIntersecting) document.body.classList.remove("sticky");
   },
